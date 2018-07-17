1b120388724ab908766cf64ad2df7ca5b23197d2
Irma <irma.kaninda@gmail.com>
2018-07-17 17:20:44 +0200

    routes and controllers setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
+
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :items
   resources :carts do
-    resources :cart_items, only:[ :new, :create, :destroy ]
+    resources :cart_items, only:[ :create, :destroy ]
   end
   resources :packs do
     resources :likes, only: [ :create, :destroy ]
     resources :favorites, only: [ :create, :destroy ]
-    resources :pack_items, only:[ :new, :create, :destroy ]
+    resources :pack_items, only:[ :create, :destroy ]
   end
 end
