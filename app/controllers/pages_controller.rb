5feb743efe59acfb06e3a1cf15325ff3576cea11
Irma <irma.kaninda@gmail.com>
2018-07-19 14:21:10 +0200

    gems

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,3 +1,6 @@
+require 'open-uri'
+require 'nokogiri'
+
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,81 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+'Barbecue'
+'Légumes et fruits'
+'Boucherie'
+'Préparations/Charcuterie/ Poisson/Veggie'
+'Crémerie'
+'Boulangerie / Petit-déjeuner'
+'Chips / Amuse gueules'
+'Biscuits / Chocolats / Confiserie'
+'Epicerie'
+'Conserves et Soupes'
+'Surgelés'
+'Boissons'
+'Bière'
+'Vins & Bulles'
+'Bébé'
+'Suppléments / Régime'
+'Soins pour le corps / Parfumerie'
+'Entretien / Ménage'
+'Animaux'
+'Non-alimentaire'
+
+# USERS
+User.create(
+  email: "irma@irma.com",
+  encrypted_password: "lewagon"
+  )
+
+# ITEMS
+
+Item.create(
+  name: ,
+  section: ,
+  description: ,
+  image: ,
+  quantity: ,
+  price: ,
+  stock:
+  )
+
+Item.create(
+  name: ,
+  section: ,
+  description: ,
+  image: ,
+  quantity: ,
+  price: ,
+  stock:
+  )
+
+#PACKS
+
+Pack.create(
+  name: "Mojito royal",
+  description: ,
+  ingredients: "4 cl de rhum", "12 cl de vin mousseux", "0.5 citron vert", "1 branche de menthe fraîche", "1 c. à café de sucre de canne", "glace pilée",
+  speed: 10,
+  difficulty: 1,
+  pack_type: "recipe",
+  )
+
+Pack.create(
+  name: "Back to school",
+  description: ,
+  speed: ,
+  difficulty: ,
+  pack_type: ,
+  items: [Item.find_by(name: "crafty")],
+  )
+
+Pack.create(
+  name: ,
+  description: ,
+  speed: ,
+  difficulty: ,
+  pack_type: ,
+  )
